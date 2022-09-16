d4dfbd1df1cd1c76b0955dacfeef9f87c8556ea7
Mike Barlozewski <barlozewski.mike@gmail.com>
2022-09-16 16:57:44 +0200

    added test connection

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ require('dotenv').config();
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 8080;
+const pg = require('pg');
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`)
@@ -32,3 +33,21 @@ app
 
 
 
+//or native libpq bindings
+//var pg = require('pg').native
+
+const conString = "postgres://ukkbuzuk:j96lW0UwEMWJzgWvKPBOq_bM_lYz9656@tyke.db.elephantsql.com/ukkbuzuk" //Can be found in the Details page
+const client = new pg.Client(conString);
+client.connect(function(err) {
+  if(err) {
+    return console.error('could not connect to postgres', err);
+  }
+  client.query('SELECT NOW() AS "theTime"', function(err, result) {
+    if(err) {
+      return console.error('error running query', err);
+    }
+    console.log(result.rows[0].theTime);
+    // >> output: 2018-08-23T14:02:57.117Z
+    client.end();
+  });
+});
