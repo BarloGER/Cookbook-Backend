a22de0733d4da9891629274cbd877dcca63cebc3
Mike Barlozewski <barlozewski.mike@gmail.com>
2022-09-20 15:55:42 +0200

    refactered server

---
+++ db.js
@@ -0,0 +1,14 @@
+const { Pool } = require("pg");
+
+const connectionString = process.env.PG_CONNECTIONSTRING;
+
+const pool = new Pool({
+    // user: process.env.DB_USER,
+    // host: process.env.DB_HOST,
+    // database: process.env.DB_NAME,
+    // password: process.env.DB_PASS,
+    // port: process.env.DB_PORT,
+    connectionString,
+})
+
+module.exports = pool;
--- server.js
+++ server.js
@@ -1,53 +1,32 @@
-require('dotenv').config();
+require("dotenv").config();
 
-const express = require('express')
-const app = express()
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const pool = require("./db");
+const pg = require("pg");
 const PORT = process.env.PORT || 8080;
-const pg = require('pg');
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(cors());
 
-app
-.route('/')
-.get((req, res) => {res.status(200).send('Home')})
+app.route("/").get((req, res) => {
+  res.status(200).send("Home");
+});
 
+app.route("/recipeOverview").get((req, res) => {
+  res.status(200).send("Rezeptübersicht");
+});
 
-app
-.route('/recipeOverview')
-.get((req, res) => {res.status(200).send('Rezeptübersicht')})
+app.route("/aboutUs").get((req, res) => {
+  res.status(200).send("Über uns");
+});
 
-
-app
-.route('/aboutUs')
-.get((req, res) => {res.status(200).send('Über uns')})
-
-
-app
-.route('/recipe')
-.get((req, res) => {res.status(200).send('Rezept')})
-
-
-
-//or native libpq bindings
-//var pg = require('pg').native
-
-const conString = "postgres://ukkbuzuk:j96lW0UwEMWJzgWvKPBOq_bM_lYz9656@tyke.db.elephantsql.com/ukkbuzuk" //Can be found in the Details page
-const client = new pg.Client(conString);
-client.connect(function(err) {
-  if(err) {
-    return console.error('could not connect to postgres', err);
-  }
-  client.query('SELECT NOW() AS "theTime"', function(err, result) {
-    if(err) {
-      return console.error('error running query', err);
-    }
-    console.log(result.rows[0].theTime);
-    // >> output: 2018-08-23T14:02:57.117Z
-    client.end();
-  });
+app.route("/recipe").get((req, res) => {
+  res.status(200).send("Rezept");
 });
