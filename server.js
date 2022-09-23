694f59e0e72f7e5fc35f8025ec3cb533e129c10e
Mike Barlozewski <barlozewski.mike@gmail.com>
2022-09-23 18:16:57 +0200

    added app.delete route

--- server.js
+++ server.js
@@ -36,6 +36,7 @@ app.post("/", async (req, res) => {
     ingredients,
   } = req.body;
   console.log(req.body);
+
   if (
     !image ||
     !title ||
@@ -52,7 +53,7 @@ app.post("/", async (req, res) => {
     const {
       rows: [createdRecipe],
     } = await pool.query(
-      `INSERT INTO recipes(image, title, required_time, difficulty, author, date, description, ingredients) VALUES($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;`,
+      `INSERT INTO recipes (image, title, required_time, difficulty, author, date, description, ingredients) VALUES ($1, $2, $3, $4, $5, $6, $7, $8) RETURNING *;`,
       [
         image,
         title,
@@ -60,13 +61,40 @@ app.post("/", async (req, res) => {
         difficulty,
         author,
         date,
-        [description],
-        [ingredients],
+        description,
+        ingredients,
       ]
     );
     return res.status(201).send(createdRecipe);
   } catch (err) {
     console.log(err);
     return res.status(500).send("Something went wrong");
+  }
+});
+
+app.delete("/", async (req, res) => {
+  const { Recipe_id } = req.body;
+  if (!Recipe_id) {
+    return res.status(400).send("Bitte Rezept ID angeben.");
+  }
+  try {
+    const {
+      rows: [deletedRecipe],
+      rowCount,
+    } = await pool.query(
+      'DELETE FROM recipes WHERE "Recipe_id" = $1 RETURNING *;',
+      [Recipe_id]
+    );
+    console.log("Rezept gelöscht", deletedRecipe);
+    if (!rowCount)
+      return res
+        .status(400)
+        .send(`Kein Rezept mit der ID ${Recipe_id} gefunden.`);
+    res
+      .status(200)
+      .send(`Das Rezept mit der ID ${deletedRecipe.Recipe_id} wurde gelöscht.`);
+  } catch (err) {
+    console.log(err);
+    res.status(500).send("Etwas ist schiefgelaufen.");
   }
 });
